1c8166ac8c6932451e445d7081fc1166e8f439be
haxor09alert <chaudharyalekh@gmail.com>
2023-10-31 10:12:41 +0545

    vendor-page

---
+++ lib/screens/vendor_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class VendorPage extends StatelessWidget {
+  const VendorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
