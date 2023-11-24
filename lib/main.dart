5e0f0b8b7761b3e660f8c987b258393b8cfbf11b
Chaudhary Alekh <chaudharyalekh@gmail.com>
2023-11-24 12:05:06 +0545

    push

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:mero_discountsv2/screens/more.dart';
 import 'package:mero_discountsv2/screens/policies_sub_page/policies_page.dart';
 import 'package:mero_discountsv2/screens/profile_page/profile_page.dart';
 import 'package:mero_discountsv2/screens/settings/settings.dart';
-import 'package:mero_discountsv2/screens/sub_about/sub_about.dart';
+import 'package:mero_discountsv2/screens/about_us/sub_about/sub_about.dart';
 import 'package:mero_discountsv2/screens/vendor_about_page/about_vendor.dart';
 import 'package:mero_discountsv2/screens/vendor_about_page/vendor_review.dart';
 import 'package:mero_discountsv2/screens/vendorpage/item_description.dart';
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const  MaterialApp(
-      home: SubAboutPage(),
+      home: SubAboutPage()
     );
   }
 }
